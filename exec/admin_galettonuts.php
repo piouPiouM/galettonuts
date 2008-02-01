2553e26a3c34a5b387db08152ccf48fef55b3cfe
Mehdi Kabab <pioupioum@gmail.com>
2008-02-01 23:27:18 +0100

    Mise à jour des commentaires d'en-tête

--- exec/admin_galettonuts.php
+++ exec/admin_galettonuts.php
@@ -1,19 +1,31 @@
 <?php
 /**
+ * This file is part of Galettonuts.
+ * 
+ * Galettonuts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Galettonuts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Galettonuts. If not, see <http://www.gnu.org/licenses/gpl2.html>.
+ */
+
+/**
  * Page de configuration du plugin.
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 2.0 of the GPL license
- * that is available through the world-wide-web at the following URI:
- * http://www.gnu.org/licenses/gpl2.html.
- * You should have received a copy of the GNU General Public License
- * along with Galettonuts (LICENCE.txt); if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * @package    Galettonuts
- * @author     Mehdi Kabab
- * @license    http://www.gnu.org/licenses/gpl2.html  GPL Licence 2.0
+ * @package   Galettonuts
+ * @author    Mehdi Kabab <pioupioum@tuxfamily.org>
+ * @copyright Copyright (C) 2008 Mehdi Kabab
+ * @license   http://www.gnu.org/licenses/gpl2.html  GPL Licence 2.0
+ * @version   0.1
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
--- lang/galettonuts_fr.php
+++ lang/galettonuts_fr.php
@@ -1,4 +1,33 @@
 <?php
+/**
+ * This file is part of Galettonuts.
+ * 
+ * Galettonuts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Galettonuts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Galettonuts. If not, see <http://www.gnu.org/licenses/gpl2.html>.
+ */
+
+/**
+ * Localisation française du plugin.
+ *
+ * PHP versions 4 and 5
+ *
+ * @package   Galettonuts
+ * @author    Mehdi Kabab <pioupioum@tuxfamily.org>
+ * @copyright Copyright (C) 2008 Mehdi Kabab
+ * @license   http://www.gnu.org/licenses/gpl2.html  GPL Licence 2.0
+ * @version   0.1
+ */
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
     // A
     'avis_connexion_echec_1' => 'La connexion au serveur MySQL de votre installation Galette a &eacute;chou&eacute;.',
