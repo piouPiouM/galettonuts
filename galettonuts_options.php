abaed2ef264d5e014dddb30a9335ee2a8c4edf16
Mehdi Kabab <pioupioum@gmail.com>
2009-02-15 15:39:39 +0100

    Prendre en compte la table SQL du plugin lors d'un dump.

---
+++ galettonuts_options.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of Galettonuts.
+ * 
+ * Galettonuts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Galettonuts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Galettonuts. If not, see <http://www.gnu.org/licenses/gpl2.html>.
+ */
+
+/**
+ * Options du plugins.
+ *
+ * PHP versions 4 and 5
+ *
+ * @package   Galettonuts
+ * @author    Mehdi Kabab <pioupioum@tuxfamily.org>
+ * @copyright Copyright (C) 2008 Mehdi Kabab
+ * @license   http://www.gnu.org/licenses/gpl2.html  GPL Licence 2.0
+ * @version   0.1
+ * @link      http://pioupioum.tuxfamily.org/scripts/spip/galettonuts
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// Sauvegarder la table du plugin lors d'un dump de Spip.
+if (isset($GLOBALS['exec']) && 'export_all' == $GLOBALS['exec'])
+{
+    include_spip('base/galettonuts_tables');
+}
